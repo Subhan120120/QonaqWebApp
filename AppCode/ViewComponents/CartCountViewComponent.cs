2ed0d19e9e18d2487de5ae5dac11147c65b434f6
Subhan <huseynzadesubhan@outlook.com>
2020-11-20 03:30:49 +0400

    ShoppingCart ended

---
+++ AppCode/ViewComponents/CartCountViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QonaqWebApp.AppCode.Helpers;
+using QonaqWebApp.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QonaqWebApp.AppCode.Infrastructure;
+
+namespace QonaqWebApp.AppCode.ViewComponents
+{
+    [ViewComponent]
+    public class CartCount : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<ShoppingItem> cart = SessionHelper.GetObjectFromJson<List<ShoppingItem>>(HttpContext.Session, "cart");
+
+            int cartCount = 0;
+            if (cart != null)
+            {
+                cartCount = cart.Sum(item => item.Quantity);
+            }
+            return View(cartCount);
+        }
+    }
+}
